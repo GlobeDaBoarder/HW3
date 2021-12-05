2a37922839578a9a6f973b7d0dc7e90d0cb3958a
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2021-12-05 20:59:14 +0200

    passing function

--- HW3/Source.cpp
+++ HW3/Source.cpp
@@ -3,32 +3,62 @@
 #include <fstream>
 #include <math.h>
 
+Timer RunQuickSort(Data& d)
+{
+	Timer t;
+	t.start();
+	d.quickSort();
+	t.end();
+
+	return t;
+}
+
+void RunTests(std::ofstream& res, Timer (*f)(Data& d))
+{
+	//5 sizes of data sets 10, 100, 1 000, 10 000, 100 000
+	for (int i = 0; i < 6; ++i)
+	{
+		//20 tests per each size of data 
+		for (int j = 0; j < 21; ++j)
+		{
+			if (i == 0)
+			{
+				if (j == 0)
+				{
+					res << ',';
+					continue;
+				}
+
+				res << "test #" << j << ',';
+
+				continue;
+			}
+
+			int data_size = pow(10, i);
+			if (j == 0)
+			{
+				res << "data size " << data_size << ',';
+				continue;
+			}
+
+			Data d(data_size);
+			Timer t = (*f)(d);
+			
+
+			res << t.getTime() << ',';
+		}
+		res << '\n';
+	}
+}
 
 int main()
 {
 	//experiment #1 -- Quick Sort
 	//time measuring (in milliseconds)
 
-	std::ofstream qs_res;
-	qs_res.open("./qs_res.csv");
-
-	//5 sizes of data sets 10, 100, 1 000, 10 000, 100 000
-	for (int i = 0; i < 5; ++i)
-	{
-		//20 tests per each size of data 
-		for (int j = 0; j < 20; ++j)
-		{
-			Data d(pow(10, (i + 1))); 
-			Timer t;
-			t.start();
-			d.quickSort();
-			t.end();
-
-			qs_res << t.getTime() << ';';
-		}
-		qs_res << '\n';
-	}
-
+	std::ofstream qs_res("./qs_res.csv");
+	RunTests(qs_res, RunQuickSort);
+	qs_res.close();
 
 
 }
